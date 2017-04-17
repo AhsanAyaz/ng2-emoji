677a77f6a35b15e807d9a0aa37f261915f03a14f
Ahsan Ayaz <ahsan.ubitian@gmail.com>
2017-04-18 00:28:48 +0500

    Fixed the html sanitizing issue.

--- src/ng2-emoji.pipe.ts
+++ src/ng2-emoji.pipe.ts
@@ -1,14 +1,18 @@
-import {Pipe, PipeTransform} from '@angular/core';
-import {Ng2EmojiService} from './ng2-emoji.service';
+import { Pipe, PipeTransform } from '@angular/core';
+import { Ng2EmojiService } from './ng2-emoji.service';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Pipe({
   name: 'emojis'
 })
 export class Ng2EmojiPipe implements PipeTransform{
-  transform(value: string, args: any[]) {
+
+  constructor(private _sanitizer: DomSanitizer){}
+
+  transform(value: string, args: any[]): SafeHtml {
     value = value + ''; // make sure it's a string
-    return value.replace(Ng2EmojiService.emojisRegex, function (match, text) {
+    return this._sanitizer.bypassSecurityTrustHtml(value.replace(Ng2EmojiService.emojisRegex, function (match, text) {
         return `<i class="emoji icon-ng2_em_${text}" title=":${text}:" style="display:inline-block;"></i>`;
-    });
+    }));
   }
 }
